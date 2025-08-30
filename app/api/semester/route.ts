c005985ffc2ab0f6ef50efbe7ead9e0603413462
AbdelkbirNA <abdlekbir.nainiaa12@gmail.com>
2025-08-30 17:41:37 +0100

    Base done dont touch aw9

--- app/api/semester/route.ts
+++ app/api/semester/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const fields = await prisma.fields.findMany(); // <-- récupère toutes les lignes
+    const fields = await prisma.user.findMany(); // <-- récupère toutes les lignes
     return NextResponse.json(fields);
   } catch (error) {
     console.error("Error fetching fields:", error);
