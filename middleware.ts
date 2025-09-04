b726fa240996f8956b18b73e8e29b6f000a5d331
AbdelkbirNA <abdlekbir.nainiaa12@gmail.com>
2025-09-04 18:32:03 +0100

    redirection l3ar

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,37 @@
 import { type NextRequest } from 'next/server';
 import { handleAuth } from './lib/middlewares/authMiddleware'
 import { handleadminAuth } from './lib/middlewares/adminMiddleware'
+
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const host = request.headers.get('host');
 
-  // If the route concerns authentication or protected API routes, apply the auth middleware
-  if (pathname.startsWith('/login') ) {
-      return handleAuth(request)
+  // If the host is the dashboard subdomain, apply admin auth logic
+  if (host === 'dashboard.eduhubcenter.online') {
+    return handleadminAuth(request);
+  }
+
+  // Original middleware logic for paths
+  if (pathname.startsWith('/login')) {
+    return handleAuth(request)
   }
   if (pathname.startsWith('/dashboard')) {
     return handleadminAuth(request);
   }
 
+  // No middleware action for other paths
   return;
 }
 
-  export const config = {
+export const config = {
   matcher: [
-    '/login',
-    '/dashboard/:path*', // ✅ toutes les pages admin
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
-
